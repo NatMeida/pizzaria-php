20846df2514cb7c9252de9f69a10bb7da8a3a571
AmeMeida <ame.pistache@gmail.com>
2023-09-21 18:42:06 -0300

    htmx added

--- components/head.php
+++ components/head.php
@@ -3,5 +3,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
     <link rel="stylesheet" href="./styles/style.css">
+    <script defer src="https://unpkg.com/htmx.org@1.9.5"></script>
     <title><?= $title ?></title>
 </head>
